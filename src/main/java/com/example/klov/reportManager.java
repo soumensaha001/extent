ed8dcd45ae777823d42ece6124a65d4684ab3b88
Soumen Saha2 <sahasou2@in.ibm.com>
2022-04-12 09:02:24 +0530

    updated

--- src/main/java/com/example/klov/reportManager.java
+++ src/main/java/com/example/klov/reportManager.java
@@ -2,42 +2,29 @@ package com.example.klov;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import org.json.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
 
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
-import java.util.concurrent.CountDownLatch;
-import org.springframework.stereotype.Component;
-@EnableRabbit 
-@SpringBootApplication
-@Controller
-@CrossOrigin("*")
-public class KlovApplication {
+
+@Component
+public class reportManager {
 	ExtentTest test;
 	ExtentReports report;
-	public static void main(String[] args) {
-		SpringApplication.run(KlovApplication.class, args);
-	}
-	@PostMapping("/reporting")
-	public  @ResponseBody byte[] reportResponse(@RequestBody String json) throws Exception {
-		// Parsing Service-Module json 
+	@RabbitListener(queues={"${queue.name}"})
+	public void consumeMessage(@Payload String fileBody) throws ParseException, IOException {
+		System.out.println(fileBody);
 		   JSONParser parser = new JSONParser();
-		   Object obj = parser.parse(json);
+		   Object obj = parser.parse(fileBody);
 		   org.json.simple.JSONObject jsonObject = (org.json.simple.JSONObject) obj;
 		   String testcasename= jsonObject.get("testname").toString();
 		   org.json.simple.JSONArray steps = (org.json.simple.JSONArray) jsonObject.get("steps");
@@ -70,8 +57,7 @@ public class KlovApplication {
 	        // to avoid memory leakage
 	        fl.close();
 	 
-	        // Returning above byte array
-	        return arr;
-	}
 
+	}
+	
 }
